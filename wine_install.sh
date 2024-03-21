4163c0c6a50d103629469023e68f67486b0de958
ecorbisierSimplon <ecorbisier.simplon@gmail.com>
2024-03-21 10:46:48 +0100

    Eric/Add wine install sh

--- wine_install.sh
+++ wine_install.sh
@@ -1,49 +1,19 @@
 # Install wine
+#> chmod +x wine_install.sh
+#> sudo ./wine_install.sh
 
-1
-
-```bash
 sudo dpkg --add-architecture i386
-```
 
-2
-
-```bash
 sudo apt-key add winehq.key
-```
 
-3
-
-```bash
 sudo apt-add-repository 'https://dl.winehq.org/wine-builds/ubuntu/'
-```
 
-4
-
-```bash
 sudo apt install libgd3:i386
-```
 
-5
-
-```bash
 sudo apt update
-```
 
-6
-
-```bash
 sudo apt install winehq-stable
-```
 
-7
-
-```bash
 wine --version
-```
 
-8
-
-```bash
 winecfg
-```
